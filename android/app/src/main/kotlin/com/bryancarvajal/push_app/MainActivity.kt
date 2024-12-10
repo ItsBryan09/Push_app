7b0c3d9d1bfb3bab0f27bc9ad075bc5ce18f0d7d
bryan <bryancarvajal747@gmail.com>
2024-12-10 10:11:35 -0600

    notificacion dentro de la app

---
+++ android/app/src/main/kotlin/com/bryancarvajal/push_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bryancarvajal.push_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/presentation/blocs/notifications/notifications_event.dart
@@ -0,0 +1,17 @@
+part of 'notifications_bloc.dart';
+
+abstract class NotificationsEvent {
+  const NotificationsEvent();
+}
+
+class NotificationStatusChanged extends NotificationsEvent {
+  final AuthorizationStatus status;
+  NotificationStatusChanged(this.status);
+}
+
+//TODO 2: NotificationReceived # PushMessage
+
+class NotificationReceived extends NotificationsEvent {
+  final PushMessage pushMessage;
+  NotificationReceived(this.pushMessage);
+}
